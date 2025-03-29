f429fc94489434884a830d5da7fbac71a9c73f86
Alaede Increase-Chris <increasechris124@gmail.com>
2025-03-29 10:51:33 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,11 @@ import { FileRecord } from "@/types";
 
 export default function Home() {
   const [pdfs, setPdfs] = useState<FileRecord[]>([]);
-  const [downloadedPdfs, setDownloadedPdfs] = useState<FileRecord[]>([]);
   const router = useRouter();
 
   useEffect(() => {
     const storedFiles = JSON.parse(localStorage.getItem("pdfs") || "[]");
     setPdfs(storedFiles);
-    const downloadedFiles = JSON.parse(
-      localStorage.getItem("downloaded-pdfs") || "[]"
-    );
-    setDownloadedPdfs(downloadedFiles);
   }, []);
 
   const handleClick = (pdf: {
@@ -32,7 +27,7 @@ export default function Home() {
   return (
     <div className="p-6">
       <div className="flex justify-between items-center mb-5">
-        <h1 className="text-2xl font-bold">Uploaded PDFs</h1>
+        <h1 className="text-2xl font-bold">Recently Uploaded PDFs</h1>
         <Button
           className={`w-fit dark:bg-white dark:text-[#333] bg-gray-500 text-white cursor-pointer`}
           onClick={() => {
@@ -53,7 +48,7 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {pdfs.map((pdf, index) => (
+            {pdfs.slice(0, 4).map((pdf, index) => (
               <tr key={index} className="">
                 <td className="border border-gray-300 px-4 py-2 text-center">
                   {index + 1}
@@ -75,42 +70,6 @@ export default function Home() {
         </table>
       ) : (
         <p className="text-gray-500">No PDFs uploaded yet.</p>
-      )}
-      <div className="flex justify-between items-center my-5">
-        <h1 className="text-2xl font-bold">Downloaded PDFs</h1>
-      </div>
-      {downloadedPdfs.length > 0 ? (
-        <table className="w-full border-collapse border border-gray-300">
-          <thead>
-            <tr className="">
-              <th className="border border-gray-300 px-4 py-2">#</th>
-              <th className="border border-gray-300 px-4 py-2">File Name</th>
-              <th className="border border-gray-300 px-4 py-2">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {downloadedPdfs.map((pdf, index) => (
-              <tr key={index} className="">
-                <td className="border border-gray-300 px-4 py-2 text-center">
-                  {index + 1}
-                </td>
-                <td className="border border-gray-300 px-4 py-2">
-                  {pdf.file_name}
-                </td>
-                <td className="border border-gray-300 px-4 py-2 text-center">
-                  <button
-                    onClick={() => handleClick(pdf)}
-                    className="text-blue-500 hover:underline curosr-pointer"
-                  >
-                    View PDF
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p className="text-gray-500">No PDFs downloaded yet.</p>
       )}
     </div>
   );
